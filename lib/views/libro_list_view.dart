cdfa5842703e9117e955d7575b1fa4e39788e52f
Casp06 <cristophersp706@gmail.com>
2025-08-04 18:01:01 -0400

    Update libro_list_view.dart

--- lib/views/libro_list_view.dart
+++ lib/views/libro_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../controllers/libro_controller.dart';
 import '../widgets/libro_card.dart';
+import '../models/libro.dart';
 
 class LibroListView extends StatefulWidget {
   const LibroListView({Key? key}) : super(key: key);
@@ -11,25 +12,54 @@ class LibroListView extends StatefulWidget {
 
 class _LibroListViewState extends State<LibroListView> {
   final LibroController _controller = LibroController();
+  List<Libro> _libros = [];
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _controller.cargarLibros();
+    _cargarLibros();
+  }
+    Future<void> _cargarLibros() async {
+    final libros = await _controller.obtenerLibros();
+    setState(() {
+      _libros = libros;
+      _isLoading = false;
+    });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final libros = _controller.libros;
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Mi Biblioteca')),
-      body: ListView.builder(
-        itemCount: libros.length,
-        itemBuilder: (context, index) {
-          return LibroCard(libro: libros[index]);
-        },
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('Mi Biblioteca')),
+    body: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              final resultado = await Navigator.pushNamed(context, '/nuevoLibro');
+              if (resultado == true) {
+                _cargarLibros();
+              }
+            },
+            child: Text('Agregar Libro'),
+          ),
+        ),
+        Expanded(
+          child: _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : _libros.isEmpty
+                  ? Center(child: Text('No hay libros disponibles'))
+                  : ListView.builder(
+                      itemCount: _libros.length,
+                      itemBuilder: (context, index) {
+                        return LibroCard(libro: _libros[index]);
+                      },
+                    ),
+        ),
+      ],
+    ),
+  );
+}
 }
