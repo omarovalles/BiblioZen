2163ba2bd97aeb5e277e2b01974d12cf1b403d9f
Casp06 <cristophersp706@gmail.com>
2025-08-04 17:55:37 -0400

    Update libro.dart

--- lib/models/libro.dart
+++ lib/models/libro.dart
@@ -6,8 +6,8 @@ class Libro {
   String titulo;
   String autor;
   String genero;
-  String estado;
-  String fechaRegistro;
+  bool estado;
+  DateTime fechaRegistro;
 
   Libro({
     this.id,
@@ -19,14 +19,17 @@ class Libro {
   });
 
   Map<String, dynamic> toMap() {
-    return {
-      'id': id,
+final map = <String, dynamic>{
       'titulo': titulo,
       'autor': autor,
       'genero': genero,
       'estado': estado,
-      'fechaRegistro': fechaRegistro,
+      'fechaRegistro': fechaRegistro.toIso8601String(),
     };
+    if (id != null) {
+      map['id'] = id;
+    }
+    return map;
   }
 
   factory Libro.fromMap(Map<String, dynamic> map) {
@@ -35,8 +38,8 @@ class Libro {
       titulo: map['titulo'],
       autor: map['autor'],
       genero: map['genero'],
-      estado: map['estado'],
-      fechaRegistro: map['fechaRegistro'],
+      estado: map['estado'] == 1,
+      fechaRegistro: DateTime.parse(map['fechaRegistro']),
     );
   }
 }
